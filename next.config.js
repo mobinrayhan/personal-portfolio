6a0ba393c7deb278a7bfbad8457252de239277fc
Mobin <contact@mobin.dev>
2025-08-23 00:14:47 +0600

    added offline issues

--- next.config.js
+++ next.config.js
@@ -9,78 +9,9 @@ const nextConfig = {
   },
 };
 
-const runtimeCaching = [
-  {
-    urlPattern: /^https:\/\/mobin\.dev\/.*\.(png|jpg|jpeg|webp|svg|gif|ico)$/,
-    handler: "CacheFirst",
-    options: {
-      cacheName: "image-cache",
-      expiration: {
-        maxEntries: 200,
-        maxAgeSeconds: 60 * 24 * 60 * 60, // 60 Days
-      },
-      cacheableResponse: {
-        statuses: [0, 200],
-      },
-    },
-  },
-  {
-    urlPattern: /^https:\/\/mobin\.dev\/_next\/image\?.*$/,
-    handler: "CacheFirst",
-    options: {
-      cacheName: "next-image-cache",
-      expiration: {
-        maxEntries: 100,
-        maxAgeSeconds: 60 * 24 * 60 * 60,
-      },
-      cacheableResponse: {
-        statuses: [0, 200],
-      },
-    },
-  },
-  {
-    urlPattern: /^https:\/\/mobin\.dev\/_next\/static\/.*/i,
-    handler: "CacheFirst",
-    options: {
-      cacheName: "next-static",
-      expiration: {
-        maxEntries: 100,
-        maxAgeSeconds: 365 * 24 * 60 * 60, // 1 year
-      },
-      cacheableResponse: {
-        statuses: [0, 200],
-      },
-    },
-  },
-  {
-    urlPattern: /^https:\/\/fonts\.(?:gstatic|googleapis)\.com\/.*/i,
-    handler: "CacheFirst",
-    options: {
-      cacheName: "google-fonts",
-      expiration: {
-        maxEntries: 30,
-        maxAgeSeconds: 365 * 24 * 60 * 60,
-      },
-    },
-  },
-  {
-    urlPattern: /^https:\/\/mobin\.dev\/.*/i,
-    handler: "NetworkFirst", // so updates come when online
-    options: {
-      cacheName: "pages-cache",
-      networkTimeoutSeconds: 3,
-      expiration: {
-        maxEntries: 50,
-        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 Days
-      },
-    },
-  },
-];
-
 export default withPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-  runtimeCaching,
+  dest: "public", // destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // disable PWA in the development environment
+  register: true, // register the PWA service worker
+  skipWaiting: true, // skip waiting for service worker activation
 })(nextConfig);
